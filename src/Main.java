a9cbc957e4e1a0ae7f769d85bae0b9bc00ae0d7e
daniel.gotlieb <dgotlieb8@gmail.com>
2018-02-08 16:37:18 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\daniel.gotlieb\\Desktop\\Latest web drivers\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void openTranslate(){
+        driver.get("https://translate.google.com/");
+    }
+}
